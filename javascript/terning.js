591a3012b5b7ab9a3db96a573bcc5c94e0f906e1
emil584c <emilwaellind@gmail.com>
2022-09-07 18:29:40 +0200

    terningspil

--- javascript/terning.js
+++ javascript/terning.js
@@ -1,8 +1,9 @@
 var diceRolls = [];
+var allDiceRolls = [];
+var clickCount = 0;
 
 
-
-// function der laver terningkast og skubber resultat til array
+// function der laver terningkast
 function rollDice(){
     var die1 = document.getElementById("die1");
     var die2 = document.getElementById("die2");
@@ -12,44 +13,51 @@ function rollDice(){
     var diceTotal = d1 + d2;
     die1.innerHTML = d1;
     die2.innerHTML = d2;
-    status.innerHTML = "DU SLOG" + " " + diceTotal + "."
+    status.innerHTML = "DU SLOG" + " " + diceTotal
     var result = d1 + d2;
 
+
+    // her skubber jeg resultaterne til arrays
+    allDiceRolls.push(result)
     if (d1 + d2 > 9){
         diceRolls.push(result)
-        document.getElementById("dingding").innerHTML = " ";
-    }else
-        document.getElementById("dingding").innerHTML = "Prøv igen!";
+    }
 
     document.getElementById("throw").innerHTML="REPEAT"
+
+
+    // Her tæller jeg hvor mange slag der er blevet taget og giver en udmeldelse hvis alle er blevet brugt og om man har vundet
+    clickCount ++;
+    if (clickCount == 10 && diceRolls.length < 3){
+        document.getElementById("status").innerHTML="du har brugt alle dine slag og har desværre ikke vundet, men prøv igen i morgen!";
+        document.getElementById("throw").style.display = "none";
+        die1.style.display = "none";
+        die2.style.display = "none";
+        document.getElementById("resultater").style.display = "flex";
+    }else if (clickCount == 10 && diceRolls.length > 2){
+        document.getElementById("status").innerHTML="SÅ FOR SATAN DER ER GEVINST, BRUG RABATKODE E94BQ FOR 20% AF ET MÅNEDS ABONNEMENT";
+        document.getElementById("throw").style.display = "none";
+        die1.style.display = "none";
+        die2.style.display = "none";  
+        document.getElementById("resultater").style.display = "flex";  
+    }
 }
 
 
 // function der printer arrayet
 function showResults(){
-    for (var i = 0; i < 5; i++){
-        document.getElementById("resultat").innerHTML+= "<br>" + diceRolls[i];
+    for (var i = 0; i < 10; i++){
+        document.getElementById("allthrows").innerHTML+= " " + allDiceRolls[i];
     }
-}
-
-// alert der siger om man har vundet hvis der er mere end 2 ting i arrayet
-function winner(){
-    if (diceRolls.length > 2)
-        alert("DU VANDT FANDME!!!");
-    else
-        alert("kom igen, prøv igen i morgen");
+    document.getElementById("resultater").style.display = "none";
 }
 
 
 
-// Her tæller jeg hvor mange slag der er blevet taget og giver en udmeldelse hvis alle er blevet brugt
-var clickCount = 0;
-function clickAmount(){
-    clickCount ++;
-    if (clickCount > 10){
-        alert("du har sgu brugt alle dine slag!")
-    };
-}
+
+
+
+
 
 
 
