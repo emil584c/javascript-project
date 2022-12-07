431144647e7d5a6cb01d376f3f2f229f14a76441
YoungChop96 <105641588+YoungChop96@users.noreply.github.com>
2022-12-07 09:03:16 +0100

    Update script-header.js

--- javascript/script-header.js
+++ javascript/script-header.js
@@ -1,66 +1,69 @@
 window.addEventListener("load", lol);
 
-
+// Declarations 
 function lol() {
-    const btnHamburger = document.querySelector('#btnHamburger');
-    const body = document.querySelector('body');
-    const header = document.querySelector('.header');
-    const overlay = document.querySelector('.overlay');
-    const fadeElements = document.querySelectorAll('.has-fade')
-    const hbmenu = document.querySelector ('#hbmenu')
+  const btnHamburger = document.querySelector('#btnHamburger');
+  const body = document.querySelector('body');
+  const header = document.querySelector('.header');
+  const overlay = document.querySelector('.overlay');
+  const fadeElements = document.querySelectorAll('.has-fade')
+  const hbmenu = document.querySelector('#hbmenu')
 
-    btnHamburger.addEventListener('click', function () {
-        console.log('click hamburger');
-
-        if (header.classList.contains('open')) { // Close Hamburger menu
-            body.classList.remove('noscroll');
-            header.classList.remove('open');
-            fadeElements.forEach(function(element){
-            element.classList.remove('fade-in');
-            element.classList.add('fade-out');
-            });
-
-        }
-
-        else { // Open Hamburger Menu
-            body.classList.add('noscroll');
-            header.classList.add('open');
-            fadeElements.forEach(function(element){
-                element.classList.remove('fade-out');
-                element.classList.add('fade-in');
-            });
-
-        }
-
-    });
-
-    hbmenu.addEventListener('click', function () {
-        console.log('click hb link');
+  btnHamburger.addEventListener('click', function () {
+    console.log('click hamburger');
 
 
-        if (header.classList.contains('open')) { // Close Hamburger menu on link
-            body.classList.remove('noscroll');
-            header.classList.remove('open');
-            fadeElements.forEach(function(element){
-            element.classList.remove('fade-in');
-            element.classList.add('fade-out');
-            });
+    // Close Hamburger menu
+    if (header.classList.contains('open')) {
+      body.classList.remove('noscroll');
+      header.classList.remove('open');
+      fadeElements.forEach(function (element) {
+        element.classList.remove('fade-in');
+        element.classList.add('fade-out');
+      });
 
-        }
+    }
 
-    });
+    // Open Hamburger Menu
+    else {
+      body.classList.add('noscroll');
+      header.classList.add('open');
+      fadeElements.forEach(function (element) {
+        element.classList.remove('fade-out');
+        element.classList.add('fade-in');
+      });
+
+    }
+
+  });
+
+  // Close Hamburger menu on link
+  hbmenu.addEventListener('click', function () {
+    console.log('click hb link');
+
+
+    if (header.classList.contains('open')) {
+      body.classList.remove('noscroll');
+      header.classList.remove('open');
+      fadeElements.forEach(function (element) {
+        element.classList.remove('fade-in');
+        element.classList.add('fade-out');
+      });
+
+    }
+
+  });
 
 };
 
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+// toggle between hiding and showing the dropdown content
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
-onclick = function(event) {
+// Close the dropdown menu if click outside of it
+onclick = function (event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
@@ -72,9 +75,3 @@ onclick = function(event) {
     }
   }
 }
-
-
-
-// ----------------------------------------------------------------------
-
-
